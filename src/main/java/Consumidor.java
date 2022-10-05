a9444836030d588e086b5139780a56fa89427d01
Alissia Lima <alissialima@hotmail.com>
2022-10-05 05:25:34 -0300

    feat: preparacao

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -3,27 +3,47 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
+import java.nio.charset.StandardCharsets;
+
 
 public class Consumidor {
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray ()) {
+            if (ch == '.') Thread.sleep (1000);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
+
+
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
+
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "plica"
-                + "";
+        String NOME_FILA = "hello";
         canal.queueDeclare(NOME_FILA, false, false, false, null);
 
         DeliverCallback callback = (consumerTag, delivery) -> {
-            String mensagem = new String(delivery.getBody());
-            System.out.println("Eu " + consumerTag + " Recebi: " + mensagem);
+            String mensagem = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println ("[x] Recebido '" + mensagem + "'");
+            try {
+                try {
+                    doWork (mensagem);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } finally {
+                System.out.println ("[x] Feito");
+            }
         };
+        boolean autoAck = true; // ack é feito aqui. Como está autoAck, enviará automaticamente
+        canal.basicConsume (NOME_FILA, autoAck, callback, consumerTag -> {});
 
-        // fila, noAck, callback, callback em caso de cancelamento (por exemplo, a fila foi deletada)
-        canal.basicConsume(NOME_FILA, true, callback, consumerTag -> {
-            System.out.println("Cancelaram a fila: " + NOME_FILA);
-        });
     }
 }
 
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -6,19 +6,24 @@ public class Produtor {
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("mqadmin");
+        connectionFactory.setPassword("Admin123XX_");
+
         try (
                 Connection connection = connectionFactory.newConnection();
                 Channel canal = connection.createChannel();
         ) {
-            String mensagem = "Olá";
-            String NOME_FILA = "plica";
+            String mensagem = "Ola";
+            String NOME_FILA = "hello";
 
             //(queue, passive, durable, exclusive, autoDelete, arguments)
             canal.queueDeclare(NOME_FILA, false, false, false, null);
 
             // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
             canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
-
+            System.out.println ("[x] Enviado '" + mensagem + "'");
         }
     }
 }
