0623ec0b02853d75889e10fc22198a5808599b64
Alissia Lima <alissialima@hotmail.com>
2022-10-05 20:42:21 -0300

    feat: expedicao r-r, ack, durabilidade, envio justo, juntado td

--- src/main/java/Consumidor.java
+++ src/main/java/Consumidor.java
@@ -7,15 +7,21 @@ import java.nio.charset.StandardCharsets;
 
 
 public class Consumidor {
+    private static final String NOME_FILA  = "task_queue";
 
-    private static void doWork(String task) throws InterruptedException {
+    private static void doWork(String task) {
         for (char ch: task.toCharArray ()) {
-            if (ch == '.') Thread.sleep (1000);
+            if (ch == '.') {
+                try {
+                    Thread.sleep (1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
     }
 
     public static void main(String[] args) throws Exception {
-
 
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("localhost");
@@ -25,23 +31,23 @@ public class Consumidor {
         Connection conexao = connectionFactory.newConnection();
         Channel canal = conexao.createChannel();
 
-        String NOME_FILA = "hello";
-        canal.queueDeclare(NOME_FILA, false, false, false, null);
+        boolean duravel = true;
+        canal.queueDeclare(NOME_FILA, duravel, false, false, null);
+
+        int prefetchCount = 1;
+        canal.basicQos(prefetchCount);
 
         DeliverCallback callback = (consumerTag, delivery) -> {
             String mensagem = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println ("[x] Recebido '" + mensagem + "'");
             try {
-                try {
-                    doWork (mensagem);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                doWork (mensagem);
             } finally {
                 System.out.println ("[x] Feito");
+                canal.basicAck(delivery.getEnvelope(). getDeliveryTag(), false);
             }
         };
-        boolean autoAck = true; // ack é feito aqui. Como está autoAck, enviará automaticamente
+        boolean autoAck = false;
         canal.basicConsume (NOME_FILA, autoAck, callback, consumerTag -> {});
 
     }
--- src/main/java/Produtor.java
+++ src/main/java/Produtor.java
@@ -1,8 +1,11 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
 
 public class Produtor {
+
+    private static final String NOME_FILA  = "task_queue";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
@@ -15,14 +18,14 @@ public class Produtor {
                 Connection connection = connectionFactory.newConnection();
                 Channel canal = connection.createChannel();
         ) {
-            String mensagem = "Ola";
-            String NOME_FILA = "hello";
+            String mensagem = "Hello Alissia Deolinda Oliveira de Lima Bye";
 
+            boolean duravel = true;
             //(queue, passive, durable, exclusive, autoDelete, arguments)
-            canal.queueDeclare(NOME_FILA, false, false, false, null);
+            canal.queueDeclare(NOME_FILA, duravel, false, false, null);
 
             // ​(exchange, routingKey, mandatory, immediate, props, byte[] body)
-            canal.basicPublish("", NOME_FILA, false, false, null, mensagem.getBytes());
+            canal.basicPublish("", NOME_FILA, false, false, MessageProperties.PERSISTENT_TEXT_PLAIN, mensagem.getBytes());
             System.out.println ("[x] Enviado '" + mensagem + "'");
         }
     }
